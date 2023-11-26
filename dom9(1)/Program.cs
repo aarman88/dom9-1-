052f0ce3cc3917ac7cce5022f5153311fadc99af
Арман <@ARMAN>
2023-11-26 22:04:43 +0600

    Добавьте файлы проекта.

---
+++ dom9(1)/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+public class Employee
+{
+    protected string name;
+    protected int age;
+    protected double salary;
+
+    public Employee(string name, int age, double salary)
+    {
+        this.name = name;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    // Метод для вывода информации о сотруднике
+    public void GetInfo()
+    {
+        Console.WriteLine($"Имя: {name}, Возраст: {age}, Зарплата: {salary}");
+    }
+
+    // Метод для расчета годовой зарплаты
+    public virtual double CalculateAnnualSalary()
+    {
+        return salary * 12;  // Простой расчет годовой зарплаты (без учета налогов и бонусов)
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Пример использования базового класса Employee
+        Employee employee = new Employee("John Doe", 30, 50000);
+        employee.GetInfo();
+
+        double annualSalary = employee.CalculateAnnualSalary();
+        Console.WriteLine($"Годовая зарплата: {annualSalary}");
+    }
+}
